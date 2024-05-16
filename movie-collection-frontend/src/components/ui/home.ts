3209330f7680f80b3cc9603f0bbd09009b3e8776
Gustaf Ekström <gusek93@gmail.com>
2024-05-16 22:57:32 +0200

    fix project structure

---
+++ movie-collection-frontend/src/components/ui/home.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ImageMovie = styled.img`
+  height: 350px;
+`;
+export const MovieImageContainer = styled.div`
+  width: 1065px;
+  display: flex;
+`;
---
+++ movie-collection-frontend/src/components/ui/movieCarousel.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const NewMoviesDisplay = styled.div`
+  height: 470px;
+  width: 1579px;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+  transition: scroll-behavior 0.5s ease;
+`;
+export const Carousel = styled.div`
+  height: 600px;
+  
+  overflow: visible;
+`;
