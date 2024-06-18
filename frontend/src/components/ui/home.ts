66363a56187a2d7ad4d8527b56dee1f7c3849070
Gustaf Ekström <gusek93@gmail.com>
2024-06-18 14:28:15 +0200

    isolate frontend in branch

--- frontend/src/components/ui/home.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const ImageMovie = styled.img`
-  height: 350px;
-`;
-export const MovieImageContainer = styled.div`
-  width: 1065px;
-  display: flex;
-`;
